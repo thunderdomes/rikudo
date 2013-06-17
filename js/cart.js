75c5e3d04a542334f8ebf61b38ce4d8cea302664
Arie <arie@wego.com>
2013-06-17 23:17:12 +0700

    optimation JS and CSS

--- js/cart.js
+++ js/cart.js
@@ -1,42 +1,6 @@
-/**
- *   Set decimal point
- */	
-function appSetDecimalPoint(num){
-	var num_ = (num != null) ? num.toString() : "";
-	var currency_format = document.getElementById("hid_currency_format").value;
-	
-	if(currency_format == 'european'){
-		num_ = num_.replace(".", ",");
-		return num_;
-	}
-	return num_;
-}
-
-/**
- *   Update total sum of shopping cart
- */	
-function appUpdateTotalSum(ind, num, total_extras){
-	arrExtrasSelected[ind] = num;
-	
-	var booking_initial_fee = document.getElementById("hid_booking_initial_fee").value;
-	var vat_percent = document.getElementById("hid_vat_percent").value;
-	var vat_sum = 0;
-	var order_price = document.getElementById("hid_order_price").value;
-	var currency_symbol = document.getElementById("hid_currency_symbol").value;
-	var extras_total_sum = 0;
-	var cart_total_sum = 0;
-	
-	for(i=0; i < total_extras; i++){
-		extras_total_sum += (arrExtrasSelected[i] * arrExtras[i]);
-	}
-	
-	vat_sum = parseFloat((parseFloat(order_price) + parseFloat(booking_initial_fee) + parseFloat(extras_total_sum)) * parseFloat(vat_percent / 100));
-	cart_total_sum = parseFloat((parseFloat(order_price) + parseFloat(booking_initial_fee) + parseFloat(extras_total_sum)) + parseFloat(vat_sum));
-	
-	document.getElementById("reservation_vat").innerHTML = currency_symbol+appSetDecimalPoint(vat_sum.toFixed(2));
-	document.getElementById("reservation_total").innerHTML = currency_symbol+appSetDecimalPoint(cart_total_sum.toFixed(2));
-	
-	// show effect on changed text
-	jQuery("#reservation_vat").animate({opacity: 0.25}, 70).animate({opacity: 1}, 70);  
-	jQuery("#reservation_total").animate({opacity: 0.25}, 70).animate({opacity: 1}, 70);  	
-}
+function appSetDecimalPoint(a){var b=(a!=null)?a.toString():"";var c=document.getElementById("hid_currency_format").value;if(c=="european"){b=b.replace(".",",");
+return b;}return b;}function appUpdateTotalSum(a,d,h){arrExtrasSelected[a]=d;var f=document.getElementById("hid_booking_initial_fee").value;var g=document.getElementById("hid_vat_percent").value;
+var e=0;var c=document.getElementById("hid_order_price").value;var k=document.getElementById("hid_currency_symbol").value;var j=0;var b=0;for(i=0;i<h;i++){j+=(arrExtrasSelected[i]*arrExtras[i]);
+}e=parseFloat((parseFloat(c)+parseFloat(f)+parseFloat(j))*parseFloat(g/100));b=parseFloat((parseFloat(c)+parseFloat(f)+parseFloat(j))+parseFloat(e));document.getElementById("reservation_vat").innerHTML=k+appSetDecimalPoint(e.toFixed(2));
+document.getElementById("reservation_total").innerHTML=k+appSetDecimalPoint(b.toFixed(2));jQuery("#reservation_vat").animate({opacity:0.25},70).animate({opacity:1},70);
+jQuery("#reservation_total").animate({opacity:0.25},70).animate({opacity:1},70);}
